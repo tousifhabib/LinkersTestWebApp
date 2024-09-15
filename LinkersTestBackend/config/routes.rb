d895add99004cd51faeac955fb8710e6dcfbfb45
Tousif Habib <ts-tousif.habib@rakuten.com>
2024-09-15 23:35:03 +0900

    new: update routes config

--- LinkersTestBackend/config/routes.rb
+++ LinkersTestBackend/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  namespace :api do
+    namespace :v1 do
+      resources :addresses, only: [:index]
+      post :build_index, to: "addresses#build_index"
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
